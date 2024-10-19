4f019ea495ecdda849f94822b01a959bfaee02bd
Chris <chris284891@gmail.com>
2024-10-19 15:46:12 -0400

    changed inputs into buttons

--- logic.js
+++ logic.js
@@ -36,6 +36,10 @@ $(document).ready(function () {
   });
 });
 
+$("#clear").click(function () {
+  $("input").val("");
+})
+
 $("input").keyup(function (event) {
   if (event.keyCode == 13) {
     $("#button").click();
@@ -44,240 +48,240 @@ $("input").keyup(function (event) {
 
 $(document).on("click", "#update-up-1", function () {
   count1++;
-  $("#counter-1").html("My current count is: " + count1);
+  $("#counter-1").html("My pill count is: " + count1);
 });
 
 $(document).on("click", "#update-down-1", function () {
   if (count1 > 0) {
     count1--;
-    $("#counter-1").html("My current count is: " + count1);
+    $("#counter-1").html("My pill count is: " + count1);
   }
 });
 
 $(document).on("click", "#update-up-2", function () {
   count2++;
-  $("#counter-2").html("My current count is: " + count2);
+  $("#counter-2").html("My pill count is: " + count2);
 });
 
 $(document).on("click", "#update-down-2", function () {
   if (count2 > 0) {
     count2--;
-    $("#counter-2").html("My current count is: " + count2);
+    $("#counter-2").html("My pill count is: " + count2);
   }
 });
 
 $(document).on("click", "#update-up-3", function () {
   count3++;
-  $("#counter-3").html("My current count is: " + count3);
+  $("#counter-3").html("My pill count is: " + count3);
 });
 
 $(document).on("click", "#update-down-3", function () {
   if (count3 > 0) {
     count3--;
-    $("#counter-3").html("My current count is: " + count3);
+    $("#counter-3").html("My pill count is: " + count3);
   }
 });
 
 $(document).on("click", "#update-up-4", function () {
   count4++;
-  $("#counter-4").html("My current count is: " + count4);
+  $("#counter-4").html("My pill count is: " + count4);
 });
 
 $(document).on("click", "#update-down-4", function () {
   if (count4 > 0) {
     count4--;
-    $("#counter-4").html("My current count is: " + count4);
+    $("#counter-4").html("My pill count is: " + count4);
   }
 });
 
 $(document).on("click", "#update-up-5", function () {
   count5++;
-  $("#counter-5").html("My current count is: " + count5);
+  $("#counter-5").html("My pill count is: " + count5);
 });
 
 $(document).on("click", "#update-down-5", function () {
   if (count5 > 0) {
     count5--;
-    $("#counter-5").html("My current count is: " + count5);
+    $("#counter-5").html("My pill count is: " + count5);
   }
 });
 
 $(document).on("click", "#update-up-6", function () {
   count6++;
-  $("#counter-6").html("My current count is: " + count6);
+  $("#counter-6").html("My pill count is: " + count6);
 });
 
 $(document).on("click", "#update-down-6", function () {
   if (count6 > 0) {
     count6--;
-    $("#counter-6").html("My current count is: " + count6);
+    $("#counter-6").html("My pill count is: " + count6);
   }
 });
 
 $(document).on("click", "#update-up-7", function () {
   count7++;
-  $("#counter-7").html("My current count is: " + count7);
+  $("#counter-7").html("My pill count is: " + count7);
 });
 
 $(document).on("click", "#update-down-7", function () {
   if (count7 > 0) {
     count7--;
-    $("#counter-7").html("My current count is: " + count7);
+    $("#counter-7").html("My pill count is: " + count7);
   }
 });
 
 $(document).on("click", "#update-up-8", function () {
   count8++;
-  $("#counter-8").html("My current count is: " + count8);
+  $("#counter-8").html("My pill count is: " + count8);
 });
 
 $(document).on("click", "#update-down-8", function () {
   if (count8 > 0) {
     count8--;
-    $("#counter-8").html("My current count is: " + count8);
+    $("#counter-8").html("My pill count is: " + count8);
   }
 });
 
 $(document).on("click", "#update-up-9", function () {
   count9++;
-  $("#counter-9").html("My current count is: " + count9);
+  $("#counter-9").html("My pill count is: " + count9);
 });
 
 $(document).on("click", "#update-down-9", function () {
   if (count9 > 0) {
     count9--;
-    $("#counter-9").html("My current count is: " + count9);
+    $("#counter-9").html("My pill count is: " + count9);
   }
 });
 
 $(document).on("click", "#update-up-10", function () {
   count10++;
-  $("#counter-10").html("My current count is: " + count10);
+  $("#counter-10").html("My pill count is: " + count10);
 });
 
 $(document).on("click", "#update-down-10", function () {
   if (count10 > 0) {
     count10--;
-    $("#counter-10").html("My current count is: " + count10);
+    $("#counter-10").html("My pill count is: " + count10);
   }
 });
 
 $(document).on("click", "#update-up-11", function () {
   count11++;
-  $("#counter-11").html("My current count is: " + count11);
+  $("#counter-11").html("My pill count is: " + count11);
 });
 
 $(document).on("click", "#update-down-11", function () {
   if (count11 > 0) {
     count11--;
-    $("#counter-11").html("My current count is: " + count11);
+    $("#counter-11").html("My pill count is: " + count11);
   }
 });
 
 $(document).on("click", "#update-up-12", function () {
   count12++;
-  $("#counter-12").html("My current count is: " + count12);
+  $("#counter-12").html("My pill count is: " + count12);
 });
 
 $(document).on("click", "#update-down-12", function () {
   if (count12 > 0) {
     count12--;
-    $("#counter-12").html("My current count is: " + count12);
+    $("#counter-12").html("My pill count is: " + count12);
   }
 });
 
 $(document).on("click", "#update-up-13", function () {
   count13++;
-  $("#counter-13").html("My current count is: " + count13);
+  $("#counter-13").html("My pill count is: " + count13);
 });
 
 $(document).on("click", "#update-down-13", function () {
   if (count13 > 0) {
     count13--;
-    $("#counter-13").html("My current count is: " + count13);
+    $("#counter-13").html("My pill count is: " + count13);
   }
 });
 
 $(document).on("click", "#update-up-14", function () {
   count14++;
-  $("#counter-14").html("My current count is: " + count14);
+  $("#counter-14").html("My pill count is: " + count14);
 });
 
 $(document).on("click", "#update-down-14", function () {
   if (count14 > 0) {
     count14--;
-    $("#counter-14").html("My current count is: " + count14);
+    $("#counter-14").html("My pill count is: " + count14);
   }
 });
 
 $(document).on("click", "#update-up-15", function () {
   count15++;
-  $("#counter-15").html("My current count is: " + count15);
+  $("#counter-15").html("My pill count is: " + count15);
 });
 
 $(document).on("click", "#update-down-15", function () {
   if (count15 > 0) {
     count15--;
-    $("#counter-15").html("My current count is: " + count15);
+    $("#counter-15").html("My pill count is: " + count15);
   }
 });
 
 $(document).on("click", "#update-up-16", function () {
   count16++;
-  $("#counter-16").html("My current count is: " + count16);
+  $("#counter-16").html("My pill count is: " + count16);
 });
 
 $(document).on("click", "#update-down-16", function () {
   if (count16 > 0) {
     count16--;
-    $("#counter-16").html("My current count is: " + count16);
+    $("#counter-16").html("My pill count is: " + count16);
   }
 });
 
 $(document).on("click", "#update-up-17", function () {
   count17++;
-  $("#counter-17").html("My current count is: " + count17);
+  $("#counter-17").html("My pill count is: " + count17);
 });
 
 $(document).on("click", "#update-down-17", function () {
   if (count17 > 0) {
     count17--;
-    $("#counter-17").html("My current count is: " + count17);
+    $("#counter-17").html("My pill count is: " + count17);
   }
 });
 
 $(document).on("click", "#update-up-18", function () {
   count18++;
-  $("#counter-18").html("My current count is: " + count18);
+  $("#counter-18").html("My pill count is: " + count18);
 });
 
 $(document).on("click", "#update-down-18", function () {
   if (count18 > 0) {
     count18--;
-    $("#counter-18").html("My current count is: " + count18);
+    $("#counter-18").html("My pill count is: " + count18);
   }
 });
 
 $(document).on("click", "#update-up-19", function () {
   count19++;
-  $("#counter-19").html("My current count is: " + count19);
+  $("#counter-19").html("My pill count is: " + count19);
 });
 
 $(document).on("click", "#update-down-19", function () {
   if (count19 > 0) {
     count19--;
-    $("#counter-19").html("My current count is: " + count19);
+    $("#counter-19").html("My pill count is: " + count19);
   }
 });
 
 $(document).on("click", "#update-up-20", function () {
   count20++;
-  $("#counter-20").html("My current count is: " + count20);
+  $("#counter-20").html("My pill count is: " + count20);
 });
 
 $(document).on("click", "#update-down-20", function () {
   if (count20 > 0) {
     count20--;
-    $("#counter-20").html("My current count is: " + count20);
+    $("#counter-20").html("My pill count is: " + count20);
   }
 });
